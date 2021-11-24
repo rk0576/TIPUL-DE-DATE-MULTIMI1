71f0f94d20ea3009801915e5060def131b6a6206
rk0576 <70932901+rk0576@users.noreply.github.com>
2021-11-24 23:27:33 +0300

    Add files via upload

---
+++ problema14.py
@@ -0,0 +1,5 @@
+A=set(input())
+B=set(input())
+print('caractere ce apar in ambele siruri',A.intersection(B))
+print('caractere cel putin intr-un sir',A.union(B))
+print('caractere ce apar in prima dar in a 2a nu ',A.difference(B))
